49e197d1fabb411875883569c1f1b827242b0d88
Giang Pham <crystallizedrain0@gmail.com>
2025-03-26 14:49:35 +1000

    Add failing ReaderEndToEndTest

--- Meiryou.Tests/Acceptance/ReaderEndToEndTest.cs
+++ Meiryou.Tests/Acceptance/ReaderEndToEndTest.cs
@@ -1,15 +1,30 @@
+using Avalonia.Controls;
+using Avalonia.Headless;
+using Avalonia.Headless.NUnit;
+using Meiryou.ViewModels;
+using Meiryou.Views;
+
 namespace Meiryou.Tests.Acceptance;
 
 public class ReaderEndToEndTest
 {
+    private ReaderWindow _readerWindow;
+    private ApplicationDatabase _applicationDatabase;
+    
     [SetUp]
     public void Setup()
     {
+        _readerWindow = new ReaderWindow
+        {
+            DataContext = new ReaderWindowViewModel()
+        };
     }
 
-    [Test]
-    public void ReaderOpensTextFileAndClosesTextFile()
+    [AvaloniaTest]
+    public void ReaderLoadsReadingContentAndCloses()
     {
-        Assert.Pass();
+        _readerWindow.Show();
+        _readerWindow.LoadFile("test.txt");
+        _readerWindow.Close();
     }
 }
