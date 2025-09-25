a7d70112d0f1e49ffdf49aa84554126e90deca74
Giang Pham <crystallizedrain0@gmail.com>
2025-09-25 16:29:25 +1000

    Make GetFiles() more specific as GetTextFiles()

--- Meiryou.App/Business/IFileLocator.cs
+++ Meiryou.App/Business/IFileLocator.cs
@@ -9,16 +9,16 @@ namespace Meiryou.Business;
 public interface IFileLocator
 {
     /// <summary>
-    /// Returns a list of all files in a specified directory, searching recursively.
+    /// Returns a list of all text files in a specified directory, searching recursively.
     /// </summary>
-    /// <param name="filePath">The path to search for files.</param>
-    /// <returns>A list of files.</returns>
-    IEnumerable<FileItem> GetFiles(string filePath);
+    /// <param name="filePath">The path to search for text files.</param>
+    /// <returns>A list of text files.</returns>
+    IEnumerable<FileItem> GetTextFiles(string filePath);
    
     /// <summary>
-    /// Returns a list of all files in multiple specified directories, searching recursively.
+    /// Returns a list of all text files in multiple specified directories, searching recursively.
     /// </summary>
-    /// <param name="filePaths">A list of paths to search for files.</param>
-    /// <returns>A list of files.</returns>
-    IEnumerable<FileItem> GetFiles(IEnumerable<string> filePaths);
+    /// <param name="filePaths">A list of paths to search for text files.</param>
+    /// <returns>A list of text files.</returns>
+    IEnumerable<FileItem> GetTextFiles(IEnumerable<string> filePaths);
 }
