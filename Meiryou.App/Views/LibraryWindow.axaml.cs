4845730f89160f326d0ad16efd80c915075afef9
Giang Pham <crystallizedrain0@gmail.com>
2025-09-25 10:20:16 +1000

    Add library window view

---
+++ Meiryou.App/Views/LibraryWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Meiryou.Views;
+
+public partial class LibraryWindow : UserControl
+{
+    public LibraryWindow()
+    {
+        InitializeComponent();
+    }
+}
