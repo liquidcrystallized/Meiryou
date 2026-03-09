4cd7ab1e4c422d4464fd7d95ee1be3316a9fc9e2
Giang Pham <crystallizedrain0@gmail.com>
2026-03-09 13:07:39 +1000

    Replace default ViewLocator with the ReactiveUI variant.

--- Meiryou.App/ViewLocator.cs
+++ Meiryou.App/ViewLocator.cs
@@ -1,37 +1,15 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using Avalonia.Controls;
-using Avalonia.Controls.Templates;
 using Meiryou.ViewModels;
+using Meiryou.Views;
+using ReactiveUI;
 
 namespace Meiryou;
 
-/// <summary>
-/// Given a view model, returns the corresponding view if possible.
-/// </summary>
-[RequiresUnreferencedCode(
-    "Default implementation of ViewLocator involves reflection which may be trimmed away.",
-    Url = "https://docs.avaloniaui.net/docs/concepts/view-locator")]
-public class ViewLocator : IDataTemplate
+public class ViewLocator : IViewLocator
 {
-    public Control? Build(object? param)
+    public IViewFor ResolveView<T>(T? viewModel, string? contract = null) => viewModel switch
     {
-        if (param is null)
-            return null;
-
-        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
-
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-
-        return new TextBlock { Text = "Not Found: " + name };
-    }
-
-    public bool Match(object? data)
-    {
-        return data is ViewModelBase;
-    }
+        MenuScreenViewModel context => new MenuScreenView { DataContext = context },
+        _ => throw new ArgumentOutOfRangeException(nameof(viewModel))
+    };
 }
