e5d57ef91e419bbaf17ea0cffba71a719bda79b2
Giang Pham <crystallizedrain0@gmail.com>
2026-05-21 14:16:43 +1000

    Update frequency rank summary.

--- Meiryou.App/Models/WordStats.cs
+++ Meiryou.App/Models/WordStats.cs
@@ -7,6 +7,7 @@ public class WordStats
 
     /// <summary>
     /// How frequently this word appears in all the user's saved content.
+    /// The lower the number, the more "frequent" it is.
     /// </summary>
     public int FrequencyRank { get; set; } = -1;
 
