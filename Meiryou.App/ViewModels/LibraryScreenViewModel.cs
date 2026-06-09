e385553c3ae54fef85b141c218ef0ba272c9f9f1
Giang Pham <crystallizedrain0@gmail.com>
2026-06-09 17:54:53 +1000

    Implemented initial versions of LoadContents and ImportText for the library screen.

--- Meiryou.App/ViewModels/LibraryScreenViewModel.cs
+++ Meiryou.App/ViewModels/LibraryScreenViewModel.cs
@@ -1,5 +1,11 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Threading.Tasks;
+using Meiryou.Core.Models;
+using Meiryou.Core.Services;
 using ReactiveUI;
+using Splat;
 
 namespace Meiryou.ViewModels;
 
@@ -12,4 +18,71 @@ public class LibraryScreenViewModel : ReactiveObject, IRoutableViewModel
     public IScreen HostScreen { get; set; }
     
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+
+    public bool IsLoadingContents
+    {
+        get;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    }
+
+    public bool IsImportingText
+    {
+        get;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    }
+    
+    public ObservableCollection<ReadingContent> Contents { get; } = [];
+
+    public ReactiveCommand<Unit, Unit> LoadContentsCommand { get; }
+    public ReactiveCommand<Unit, Unit> ImportTextCommand { get; }
+    
+    private readonly IReadingContentService _readingContentService;
+    private readonly ITextImportService _textImportService;
+
+    public LibraryScreenViewModel(IReadingContentService readingContentService, ITextImportService textImportService)
+    {
+        _readingContentService = readingContentService ?? throw  new ArgumentNullException(nameof(readingContentService));
+        _textImportService = textImportService ?? throw new ArgumentNullException(nameof(textImportService));
+
+        LoadContentsCommand = ReactiveCommand.CreateFromTask(LoadContentsAsync);
+        ImportTextCommand = ReactiveCommand.CreateFromTask(ImportTextAsync);
+
+        _ = LoadContentsAsync();
+    }
+
+    //TODO: This could get slow if the user has 1000s of pieces of content. Chunk it up maybe?
+    private async Task LoadContentsAsync()
+    {
+        IsLoadingContents = true;
+        try
+        {
+            var contents = await _readingContentService.GetAllContentsAsync();
+            Contents.Clear();
+            foreach (var content in contents)
+            {
+                Contents.Add(content);
+            }
+        }
+        finally
+        {
+            IsLoadingContents = false;
+        }
+    }
+
+    //TODO: Maybe have an option to import a file OR a textbox where a user can copy text(?).
+    private async Task ImportTextAsync()
+    {
+        //TODO: Show file picker or something.
+        // Placeholder for now.
+        IsImportingText = true;
+        try
+        {
+            //TODO: Another placeholder, do this when the file picker exists or something.
+            await Task.Delay(100);
+        }
+        finally
+        {
+            IsImportingText = false;
+        }
+    }
 }
---
+++ Meiryou.Tests/App/ViewModelTests/LibraryScreenViewModelTests.cs
@@ -0,0 +1,152 @@
+using System.Reactive.Threading.Tasks;
+using Meiryou.Core.Data;
+using Meiryou.Core.Models;
+using Meiryou.Core.Services;
+using Meiryou.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ReactiveUI;
+
+namespace Meiryou.Tests.App.ViewModelTests;
+
+public class MockScreen : IScreen
+{
+    public RoutingState Router { get; } = new();
+}
+
+public class LibraryScreenViewModelTests
+{
+    private IServiceProvider _services;
+    private MeiryouDbContext _context;
+    private LibraryScreenViewModel _viewModel;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var services = new ServiceCollection();
+        
+        services.AddDbContext<MeiryouDbContext>(options =>
+            options.UseInMemoryDatabase(databaseName: "TestDb"));
+
+        services.AddScoped<ITextImportService, TextImportService>();
+        services.AddScoped<IReadingContentService, ReadingContentService>();
+
+        services.AddScoped<MainWindowViewModel>();
+        services.AddScoped<MenuScreenViewModel>();
+        services.AddScoped<LibraryScreenViewModel>();
+        services.AddScoped<ReaderScreenViewModel>();
+        services.AddScoped<SettingsScreenViewModel>();
+
+        services.AddSingleton<IScreen, MockScreen>();
+        services.AddSingleton<IViewLocator, ViewLocator>();
+        
+        services.AddSingleton<IServiceProvider>(sp => sp);
+
+        _services = services.BuildServiceProvider();
+        _context = _services.GetRequiredService<MeiryouDbContext>();
+        _viewModel = _services.GetRequiredService<LibraryScreenViewModel>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+        if (_services is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+
+    [Test]
+    public void Constructor_InitialiseWithEmptyContents()
+    {
+        Assert.That(_viewModel.Contents, Is.Not.Null);
+        Assert.That(_viewModel.Contents, Is.Empty);
+    }
+
+    [Test]
+    public void Constructor_SetIsLoadingToTrue_ThenFalse()
+    {
+        Task.Delay(100).Wait();
+        
+        Assert.That(_viewModel.IsLoadingContents, Is.False);
+    }
+
+    [Test]
+    public async Task LoadContentsCommand_LoadsContentsFromService()
+    {
+        await _context.ReadingContents.AddAsync(new ReadingContent
+        {
+            Title = "Test Content 1",
+            Content = "This is test content 1",
+            CreatedAt = DateTime.UtcNow
+        });
+        await _context.ReadingContents.AddAsync(new ReadingContent
+        {
+            Title = "Test Content 2",
+            Content = "This is test content 2",
+            CreatedAt = DateTime.UtcNow
+        });
+        await _context.SaveChangesAsync();
+
+        await _viewModel.LoadContentsCommand.Execute().ToTask();
+        
+        // Should be ordered by CreatedAt descending.
+        Assert.That(_viewModel.Contents, Has.Count.EqualTo(2));
+        Assert.That(_viewModel.Contents.First().Title, Is.EqualTo("Test Content 2"));
+        Assert.That(_viewModel.Contents.Last().Title, Is.EqualTo("Test Content 1"));
+    }
+
+    [Test]
+    public async Task LoadContentsCommand_SetsLoadingStateCorrectly()
+    {
+        bool wasLoading = false;
+        bool isLoadingAfter = false;
+
+        var subscription = _viewModel
+            .WhenAnyValue(vm => vm.IsLoadingContents)
+            .Subscribe(value =>
+            {
+                if (value)
+                {
+                    wasLoading = true;
+                }
+
+                isLoadingAfter = value;
+            });
+
+        await _viewModel.LoadContentsCommand.Execute().ToTask();
+        
+        Assert.That(wasLoading, Is.True);
+        Assert.That(isLoadingAfter, Is.False);
+
+        subscription.Dispose();
+    }
+
+    [Test]
+    public async Task ImportTextCommand_SetsImportingStateCorrectly()
+    {
+        bool wasImporting = false;
+        bool isImportingAfter = false;
+
+        var subscription = _viewModel
+            .WhenAnyValue(vm => vm.IsImportingText)
+            .Subscribe(value =>
+            {
+                if (value)
+                {
+                    wasImporting = true;
+                }
+
+                isImportingAfter = value;
+            });
+
+        await _viewModel.ImportTextCommand.Execute().ToTask();
+
+        Assert.That(wasImporting, Is.True);
+        Assert.That(isImportingAfter, Is.False);
+
+        subscription.Dispose();
+    }
+}
