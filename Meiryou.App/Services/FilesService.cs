e3a62cd34a1f85adccbf59626e02aa4e73dedabe
Giang Pham <crystallizedrain0@gmail.com>
2026-06-12 12:14:14 +1000

    Refactor FileService to not depend on Window in constructor and instead grab the current top level window directly when needed.

--- Meiryou.App/Services/FilesService.cs
+++ Meiryou.App/Services/FilesService.cs
@@ -1,21 +1,25 @@
 using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform.Storage;
 
 namespace Meiryou.Services;
 
 public class FilesService : IFilesService
 {
-    private readonly Window _target;
-
-    public FilesService(Window target)
-    {
-        _target = target;
-    }
-    
     public async Task<IStorageFile?> OpenFileAsync()
     {
-        var files = await _target.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
+        // TODO: May not work on mobile maybe(?)
+        var topLevel = TopLevel.GetTopLevel(Avalonia.Threading.Dispatcher.UIThread?.Invoke(() => 
+            Application.Current?.ApplicationLifetime as ClassicDesktopStyleApplicationLifetime)?.MainWindow);
+
+        if (topLevel?.StorageProvider is null)
+        {
+            return null;
+        }
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
         {
             Title = "Select a text file",
             AllowMultiple = false,
