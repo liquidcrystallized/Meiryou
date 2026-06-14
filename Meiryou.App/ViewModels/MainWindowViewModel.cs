c2632d146fbe124ea7d864331acfbd79e3889bd9
Giang Pham <crystallizedrain0@gmail.com>
2026-06-14 13:29:33 +1000

    Fix issue with navigating to library where the router doesn't function (can't navigate from library to anywhere else).

--- Meiryou.App/ViewModels/MainWindowViewModel.cs
+++ Meiryou.App/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Reactive;
+using Meiryou.Core.Services;
+using Meiryou.Services;
 using ReactiveUI;
 using Splat;
 
@@ -15,7 +17,11 @@ public class MainWindowViewModel : ReactiveObject, IScreen
     public MainWindowViewModel()
     {
         NavigateNextCommand = ReactiveCommand.CreateFromObservable(
-            () => Router.Navigate.Execute(Locator.Current.GetService<LibraryScreenViewModel>() ?? throw new InvalidOperationException())
+            () => Router.Navigate.Execute(new LibraryScreenViewModel(
+                this,
+                Locator.Current.GetService<IFilesService>() ?? throw new InvalidOperationException(),
+                Locator.Current.GetService<IReadingContentService>() ?? throw new InvalidOperationException(),
+                Locator.Current.GetService<ITextImportService>() ?? throw new InvalidOperationException()))
         );
     }
 }
