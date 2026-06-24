5d3fdb5e06161a2727e88c3b2a585efe017869d1
Giang Pham <crystallizedrain0@gmail.com>
2026-06-24 15:11:15 +1000

    Add unimplemented SegmentTextIntoWords function for the parsing service.

--- Meiryou.Core/Services/TextParsing/ITextParsingService.cs
+++ Meiryou.Core/Services/TextParsing/ITextParsingService.cs
@@ -2,5 +2,10 @@ namespace Meiryou.Core.Services.TextParsing;
 
 public interface ITextParsingService
 {
-    
+    /// <summary>
+    /// Break a large string into independent "word" substrings.
+    /// </summary>
+    /// <param name="text">A large string to break up.</param>
+    /// <returns>A collection of "word" strings.</returns>
+    IEnumerable<string> SegmentTextIntoWords(string text);
 }
--- Meiryou.Core/Services/TextParsing/JapaneseTextParsingService.cs
+++ Meiryou.Core/Services/TextParsing/JapaneseTextParsingService.cs
@@ -2,5 +2,8 @@ namespace Meiryou.Core.Services.TextParsing;
 
 public class JapaneseTextParsingService : ITextParsingService
 {
-    
+    public IEnumerable<string> SegmentTextIntoWords(string text)
+    {
+        throw new NotImplementedException();
+    }
 }
