5f50591081ebd198b0f2319fed18fdade0bad8a5
Giang Pham <crystallizedrain0@gmail.com>
2026-06-27 15:11:15 +1000

    Add Japanese SegmentTextIntoWords test to check that a string tokenizes into the correct list of words.

--- Meiryou.Tests/UnitTests/Core/Services/TextParsing/JapaneseTextParsingServiceTests.cs
+++ Meiryou.Tests/UnitTests/Core/Services/TextParsing/JapaneseTextParsingServiceTests.cs
@@ -9,4 +9,19 @@ public class JapaneseTextParsingServiceTests : TextParsingServiceSharedTests
     {
         return new JapaneseTextParsingService();
     }
+
+    [Test]
+    public void SegmentTextIntoWords_ShouldReturnCorrectListOfWords()
+    {
+        string sentence = "この曲がやさしいって言うんですか？何も分かっていないからそんなことを言うんですよ。弾けるものなら弾いてみなさい。";
+        List<string> expectedWords =
+        [
+            "この", "曲", "が", "やさしい", "って", "言う", "ん", "です", "か", "？", "何", "も", "分かっていない", "から", 
+            "そんな", "こと", "を", "言う", "ん", "です", "よ", "。", "弾ける", "もの", "なら", "弾いて", "みなさい", "。"
+        ];
+        
+        List<string> actualWords = Service.SegmentTextIntoWords(sentence).ToList();
+        
+        Assert.That(actualWords, Is.EqualTo(expectedWords));
+    }
 }
