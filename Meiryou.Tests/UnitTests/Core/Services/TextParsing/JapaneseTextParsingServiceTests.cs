3add4715ddac7fdbfdf251f6cc14e1bcd8b16014
Giang Pham <crystallizedrain0@gmail.com>
2026-06-27 16:19:32 +1000

    Refactor JapaneseTextParsingServiceTests to use parameterised tests.

--- Meiryou.Tests/UnitTests/Core/Services/TextParsing/JapaneseTextParsingServiceTests.cs
+++ Meiryou.Tests/UnitTests/Core/Services/TextParsing/JapaneseTextParsingServiceTests.cs
@@ -9,19 +9,25 @@ public class JapaneseTextParsingServiceTests : TextParsingServiceSharedTests
     {
         return new JapaneseTextParsingService();
     }
-
-    [Test]
-    public void SegmentTextIntoWords_ShouldReturnCorrectListOfWords()
+    
+    [Test, TestCaseSource(nameof(SegmentTextIntoWords_ShouldReturnCorrectListOfWords_TestCases))]
+    public void SegmentTextIntoWords_ShouldReturnCorrectListOfWords(string inputText, string[] expectedWords)
     {
-        string sentence = "この曲がやさしいって言うんですか？何も分かっていないからそんなことを言うんですよ。弾けるものなら弾いてみなさい。";
-        List<string> expectedWords =
-        [
-            "この", "曲", "が", "やさしい", "って", "言う", "ん", "です", "か", "？", "何", "も", "分かっていない", "から", 
-            "そんな", "こと", "を", "言う", "ん", "です", "よ", "。", "弾ける", "もの", "なら", "弾いて", "みなさい", "。"
-        ];
-        
-        List<string> actualWords = Service.SegmentTextIntoWords(sentence).ToList();
-        
+        List<string> actualWords = Service.SegmentTextIntoWords(inputText).ToList();
+    
         Assert.That(actualWords, Is.EqualTo(expectedWords));
+    }
+    private static IEnumerable<TestCaseData> SegmentTextIntoWords_ShouldReturnCorrectListOfWords_TestCases()
+    {
+        yield return new TestCaseData
+        (
+            "この曲がやさしいって言うんですか？何も分かっていないからそんなことを言うんですよ。弾けるものなら弾いてみなさい。",
+            (string[]) [
+                "この", "曲", "が", "やさしい", "って", "言う", "ん", "です", "か", "？", "何", "も", "分かっていない", "から",
+                "そんな", "こと", "を", "言う", "ん", "です", "よ", "。", "弾ける", "もの", "なら", "弾いて", "みなさい", "。"
+            ]
+        );
+
+        yield return new TestCaseData("", Array.Empty<string>());
     }
 }
