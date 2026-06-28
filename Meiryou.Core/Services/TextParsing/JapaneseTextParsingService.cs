fab6da4fb0f202a00f98d9142c0ea407b21a3679
Giang Pham <crystallizedrain0@gmail.com>
2026-06-28 21:19:53 +1000

    Add a basic method in JapaneseTextParsingService to segment text.

--- Meiryou.Core/Services/TextParsing/JapaneseTextParsingService.cs
+++ Meiryou.Core/Services/TextParsing/JapaneseTextParsingService.cs
@@ -1,9 +1,40 @@
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Ja;
+using Lucene.Net.Analysis.TokenAttributes;
+
 namespace Meiryou.Core.Services.TextParsing;
 
 public class JapaneseTextParsingService : ITextParsingService
 {
+    public JapaneseTextParsingService() { }
+
+    //TODO: The default tokenizer doesn't output text in the required format, will need
+    // a custom method to inflect and de-inflect verbs.
     public IEnumerable<string> SegmentTextIntoWords(string text)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(text)) return [];
+        
+        Tokenizer tokenizer = new JapaneseTokenizer(new StringReader(text), null, false, JapaneseTokenizerMode.NORMAL);
+        List<string> words = [];
+
+        try
+        {
+            tokenizer.Reset();
+
+            while (tokenizer.IncrementToken())
+            {
+                var charTermAttribute = tokenizer.GetAttribute<ICharTermAttribute>();
+                
+                words.Add(charTermAttribute.ToString());
+            }
+
+            tokenizer.End();
+        }
+        finally
+        {
+            tokenizer.Dispose();
+        }
+
+        return words;
     }
 }
