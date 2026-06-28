07eef5e61931c0c69b48125171fa9ebedf58f21c
Giang Pham <crystallizedrain0@gmail.com>
2026-06-28 22:29:59 +1000

    Add a LanguageType model to indicate the language being used.

---
+++ Meiryou.Core/Models/LanguageType.cs
@@ -0,0 +1,9 @@
+namespace Meiryou.Core.Models;
+
+/// <summary>
+/// If this application ever wants to expand to other parts of the CJK trio.
+/// </summary>
+public enum LanguageType
+{
+    Japanese
+}
