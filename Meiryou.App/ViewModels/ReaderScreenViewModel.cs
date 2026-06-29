65682d94c721a01c0d55a59b4459d122b6f6927d
Giang Pham <crystallizedrain0@gmail.com>
2026-06-29 14:41:09 +1000

    Remove obsolete testing functions and variables in the ReaderScreenViewModel.

--- Meiryou.App/ViewModels/ReaderScreenViewModel.cs
+++ Meiryou.App/ViewModels/ReaderScreenViewModel.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reactive;
 using System.Threading.Tasks;
@@ -13,35 +12,10 @@ namespace Meiryou.ViewModels;
 
 public class ReaderScreenViewModel : ReactiveObject, IRoutableViewModel
 {
-    //TODO: Just for testing.
-    private readonly Dictionary<string, Word> _mockDictionary = new(StringComparer.OrdinalIgnoreCase)
-    {
-        { "この", new Word { Definition = "this (something or someone close to the speaker (including the speaker), or ideas expressed by the speaker)", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "曲", new Word { Definition = "composition; piece of music; song; track (on a record)", PartOfSpeech = "Noun", FrequencyRank = 5325, FamiliarityLevel = GenerateRandomLevel() } },
-        { "が", new Word { Definition = "A particle which indicated the subject", PartOfSpeech = "?", FrequencyRank = 5, FamiliarityLevel = GenerateRandomLevel() } },
-        { "やさしい", new Word { Definition = "Multiple definitions not implemented", PartOfSpeech = "I-adjective", FrequencyRank = 521, FamiliarityLevel =  GenerateRandomLevel() } },
-        { "って言う", new Word { Definition = "Multiple definitions not implemented", PartOfSpeech = "?", FrequencyRank = 234, FamiliarityLevel = GenerateRandomLevel() } },
-        { "ん", new Word { Definition = "Multiple definitions not implemented", PartOfSpeech = "?", FrequencyRank = 42, FamiliarityLevel = GenerateRandomLevel() } },
-        { "です", new Word { Definition = "Multiple definitions not implemented", PartOfSpeech = "?", FrequencyRank = 251, FamiliarityLevel = GenerateRandomLevel() } },
-        { "か", new Word { Definition = "Multiple definitions not implemented", PartOfSpeech = "?", FrequencyRank = 4, FamiliarityLevel = GenerateRandomLevel() } },
-        { "何も", new Word { Definition = "Definition", PartOfSpeech = "PartOfSpeech", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "分かっていない", new Word { Definition = "Definition", PartOfSpeech = "PartOfSpeech", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "から", new Word { Definition = "Definition", PartOfSpeech = "PartOfSpeech", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "そんな", new Word { Definition = "Definition", PartOfSpeech = "PartOfSpeech", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "こと", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "を", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "言う", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "よ", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "弾ける", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "もの", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "なら", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } },
-        { "弾いてみなさい", new Word { Definition = "Definition", PartOfSpeech = "?", FrequencyRank = 1, FamiliarityLevel = GenerateRandomLevel() } }
-    };
-
     private readonly ITextParsingServiceFactory _textParsingServiceFactory;
     private readonly IWordService _wordService;
     
-    public IScreen HostScreen { get; set; }
+    public IScreen HostScreen { get; }
     public ObservableCollection<WordEntry> Words { get; } = [];
 
     public ReadingContent? CurrentContent
@@ -65,7 +39,6 @@ public class ReaderScreenViewModel : ReactiveObject, IRoutableViewModel
     public string Title => "ReaderScreenViewModel";
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
 
-    public ReactiveCommand<Unit, Unit> AddRandomTextCommand { get; }
     public ReactiveCommand<Unit, Unit> ClosePopupCommand { get; }
     public ReactiveCommand<Unit, Unit> NavigateBackCommand { get; }
     public ReactiveCommand<WordEntry, Unit> SelectedWordCommand { get; }
@@ -83,7 +56,6 @@ public class ReaderScreenViewModel : ReactiveObject, IRoutableViewModel
         _textParsingServiceFactory = textParsingServiceFactory;
         _wordService = wordService;
         
-        AddRandomTextCommand = ReactiveCommand.Create(AddRandomText);
         ClosePopupCommand = ReactiveCommand.Create(ClosePopup);
         NavigateBackCommand = ReactiveCommand.Create(NavigateBack);
         SelectedWordCommand = ReactiveCommand.Create<WordEntry>(SelectWord);
@@ -131,57 +103,11 @@ public class ReaderScreenViewModel : ReactiveObject, IRoutableViewModel
             });
         }
     }
-
-    //TODO: Temporary, remove/change later.
-    private void AddRandomText()
-    {
-        string[] words = { "この", "曲", "が", "やさしい", "って言う", "ん", "です", "か", "？", "何も", "分かっていない", "から", "そんな", "こと", "を", "言う", "ん", "です", "よ", "。", "弾ける", "もの", "なら", "弾いてみなさい" };
-
-    foreach (var word in words)
-        {
-            var stats = _mockDictionary.TryGetValue(word.ToLower(), out var s)
-                ? s
-                : new Word { Text = word, Definition = "No definition available", PartOfSpeech = "Unknown", FrequencyRank = -1, FamiliarityLevel =  GenerateRandomLevel() };
-            
-            Words.Add(new WordEntry
-            {
-                Word = new Word
-                {
-                    Text = word,
-                    Definition = stats.Definition,
-                    PartOfSpeech = stats.PartOfSpeech,
-                    FrequencyRank = stats.FrequencyRank,
-                    FamiliarityLevel =  stats.FamiliarityLevel,
-                },
-            });
-
-            //if (words.Last() != word)
-            //{
-            //    Words.Add(new WordEntry
-            //    {
-            //        Word = new Word { Text = " ", FamiliarityLevel = WordFamiliarityLevel.WellKnown},
-            //        //BackgroundBrush = new SolidColorBrush(backgroundColour),
-            //        //ForegroundBrush = new SolidColorBrush(foregroundColour),
-            //        IsSpace = true,
-            //    });
-            //}
-        }
-    }
     
     private void ClosePopup()
     {
         IsPopupVisible = false;
         SelectedWord = null;
-    }
-
-    //TODO: Only for testing.
-    private static WordFamiliarityLevel GenerateRandomLevel()
-    {
-        var random = new Random();
-        var levels = Enum.GetValues<WordFamiliarityLevel>();
-        var randomIndex = random.Next(levels.Length);
-
-        return levels[randomIndex];
     }
 
     private void NavigateBack()
